9e5b47513a348bf3addcc4226154f77bbb8fcaaf
Kristina <kristina.llasaite@gmail.com>
2022-03-04 16:56:53 +0200

    initial 2

--- app/src/main/java/com/example/lad1/MainActivity.java
+++ app/src/main/java/com/example/lad1/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.lad1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onMygtukas(View view) {
+        TextView tvMain = (TextView)findViewById(R.id.tvMain);
+        tvMain.setText("Hello");
+        TextView tvMain1 = (TextView)findViewById(R.id.tvMain1);
+        tvMain1.setText("WORLD");
+    }
+    public void onBtnMainColor(View view) {
+        TextView tvMain = (TextView) findViewById(R.id.tvMain);
+        tvMain.setTextColor(Color.parseColor("#FF3700B3"));
+        TextView tvMain1 = (TextView) findViewById(R.id.tvMain1);
+        tvMain1.setTextColor(Color.parseColor("#FF018786"));
     }
 }
